168a5dc7ebd7b619b3d8209909d502720b7344fa
Tanner Nielsen <tannerntannern@gmail.com>
2018-01-18 01:14:31 -0600

    Added some content to the About Me section.

--- src/ts/util.ts
+++ src/ts/util.ts
@@ -27,7 +27,7 @@ export function debounce(func: Function, wait: number, immediate?: boolean) {
  * https://gist.github.com/aurbano/4693462
  */
 export function distApprox(p1: {x: number, y: number}, p2: {x: number, y: number}): number {
-	let x = p2.x-p1.x,
-		y = p2.y-p1.y;
+	let x = p2.x - p1.x,
+		y = p2.y - p1.y;
 	return 1.426776695*Math.min(0.7071067812*(Math.abs(x)+Math.abs(y)), Math.max (Math.abs(x), Math.abs(y)));
 }
