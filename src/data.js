99665c4272e91b845c2985f6749e65ca8623cee7
Tanner Nielsen <tannerntannern@gmail.com>
2018-04-07 19:00:07 -0500

    Got the projects carousel to be at least nice enough to publish.

--- src/data.js
+++ src/data.js
@@ -36,19 +36,37 @@ module.exports = {
 		  "name": "SportsLab360",
 		  "timeFrame": "2017 - Present",
 		  "image" : "/img/projects/sportslab360.png",
-		  "shortDescription": "An online program aimed at youth soccer clubs to help increase players' tactical abilities",
-		  "longDescription": ""
+		  "shortDescription": "An online program aimed at youth soccer clubs to help refine players' tactical abilities.",
+		  "longDescription": "",
+          "links": {
+		    "Website": "https://sportslab360.com"
+          }
 	  },
       {
-        "name": "Yob",
+          "name": "Abrahamson Website",
+          "timeFrame": "Jan 2018 - Mar 2018",
+          "image": "/img/projects/abrahamson.png",
+		  "shortDescription": "A custom business website complete with a user content management system.",
+		  "longDescription": "",
+          "links": {
+            "Website": "https://abrahamsonnurseries.com"
+          }
+      },
+      {
+        "name": "Yob - Graph Editor",
         "timeFrame": "2014 - 2016",
         "image": "/img/projects/yob.png",
-        "shortDescription": "A graphical analysis add-on for Google Docs",
-        "longDescription": ""
+        "shortDescription": "A simple and streamlined graphical analysis add-on for Google Docs.",
+        "longDescription": "",
+        "links": {
+          "Installation page": "https://gsuite.google.com/marketplace/app/yob_graph_editor/31608768008",
+          "Documentation": "https://jordanhe2.github.io/Yob-Hosting/site/index.html"
+        }
       }
   ],
   contacts: {
     "Email": {"icon": "", "link": ""},
+    "LinkedIn": {"icon": "", "link": ""},
     "GitHub": {"icon": "", "link": ""}
   }
 };
