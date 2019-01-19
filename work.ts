f46b1c3fb4f0c73d6045da11e1834de5ac61e3f1
Tanner Nielsen <tannerntannern@gmail.com>
2019-01-19 00:02:13 -0600

    Started drafting a few ideas and just needed to save them somewhere.

---
+++ work.ts
@@ -0,0 +1,31 @@
+// TODO: maybe do something like https://html5up.net/paradigm-shift, but maybe with arrows
+// TODO: will need a search bar or tag filtering thing
+
+enum Type {
+	Professional, Personal
+}
+
+enum Category {
+	Website, Package, Framework
+}
+
+enum Technology {
+	HTML, Pug,
+	CSS, SASS,
+	JavaScript, TypeScript,
+	'Node.js',
+	'Vue.js', React,
+	Materialize,
+	php, Laravel, 'Laravel Voyager',
+	GameMaker
+}
+
+type Timeline = {start: string, end?: string};
+
+type Project = {
+	type: Type,
+	category: Category,
+	technologies: Technology[],
+	timeline: Timeline,
+	openSource: boolean
+}
